fd3400dbbd47187316b62d616a41ae841d601b46
candicenoel <candice29@gmail.com>
2019-09-08 23:54:03 +0000

    links

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,6 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  
   def index
     @places = Place.all
   end
@@ -9,16 +8,19 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
-  
-def create
-  current_user.places.create(place_params)
-  redirect_to root_path
-end
 
-private
+  def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
 
-def place_params
-  params.require(:place).permit(:name, :description, :address)
-end
+  def show
+    @place = Place.find(params[:id])
+  end
 
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
